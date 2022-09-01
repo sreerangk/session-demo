e60fafed8af7b10115d49b3895f2a6a6e01d879b
sreerang k <sreerangk77@gmail.com>
2022-09-01 11:30:35 +0530

    update view

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -4,5 +4,6 @@ from  . import views
 
 urlpatterns = [
     path('',views.home, name='home'),
-    path('login',views.login, name='login')
+    path('login',views.user_login, name='user_login'),
+    path('logout',views.user_logout, name='user_logout')
 ]
--- firstapp/views.py
+++ firstapp/views.py
@@ -1,12 +1,37 @@
-from django.shortcuts import render,render
-from django.contrib.auth import authenticate
-
-# Create your views here.
-def home(requist):
+from urllib import request
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login ,logout  #django authendicarion (login logout)
 
 
-    return render(requist, 'home.html')
 
 
-def login(requist):
-    return render(requist, 'login.html')
+def user_login(request):
+    # if 'username' in request.session:     /// another method session authendication 
+    if request.user.is_authenticated:   #django authendication
+        return redirect(home)
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+     
+        user = authenticate(username=username, password=password)
+        if user is not None:   # means if user is authendicated
+            #request.session['username'] = username  /// another method
+            login(request, user)
+            return redirect(home)
+        else:
+            print('invalid credentials')
+    return render(request, 'login.html')
+
+def home(request):
+    # if 'username' in request.session:     /// another method
+    if request.user.is_authenticated:
+        return render(request, 'home.html')
+   
+    return redirect(user_login)
+    
+def user_logout(request):
+    # if 'username' in request.session:     /// another method
+    if request.user.is_authenticated:
+
+        request.session.flush()
+    return redirect(user_login)
