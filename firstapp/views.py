9e339d07d5701bbf3a5fe1b3c5af652ac97259f6
sreerang k <sreerangk77@gmail.com>
2022-09-01 14:26:39 +0530

    update base

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,37 +1,38 @@
 from urllib import request
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login ,logout  #django authendicarion (login logout)
-
-
+#from django.contrib.auth.decorators import login_required  //3rd method
 
 
 def user_login(request):
-    # if 'username' in request.session:     /// another method session authendication 
-    if request.user.is_authenticated:   #django authendication
+    # if 'username' in request.session:          /// 2nd method session authendication 
+    if request.user.is_authenticated:            #django authendication
         return redirect(home)
     if request.method == 'POST':
         username = request.POST['username']
-        password = request.POST['password']
-     
+        password = request.POST['password'] 
         user = authenticate(username=username, password=password)
         if user is not None:   # means if user is authendicated
-            #request.session['username'] = username  /// another method
+            #request.session['username'] = username  /// 2nd method
             login(request, user)
             return redirect(home)
         else:
             print('invalid credentials')
     return render(request, 'login.html')
 
+#@login_required(login_url='/')                  //3rd method
 def home(request):
-    # if 'username' in request.session:     /// another method
+    # if 'username' in request.session:          /// 2nd method
     if request.user.is_authenticated:
-        return render(request, 'home.html')
+         return render(request, 'home.html')
    
     return redirect(user_login)
-    
-def user_logout(request):
-    # if 'username' in request.session:     /// another method
-    if request.user.is_authenticated:
 
-        request.session.flush()
+
+#@login_required(login_url='/')                    //3rd method
+def user_logout(request):
+    # if 'username' in request.session:           /// 2nd method
+    if request.user.is_authenticated:
+        logout(request)
+        #request.session.flush()                     /// both
     return redirect(user_login)
